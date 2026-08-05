545ab7ab468fa3b9037c387a187b1edca7c49db4
KahouJA <thehellbentkheirourgos@gmail.com>
2026-08-06 04:33:26 +0800

    Upgrade cache version and enhance activation logic
    Updated service worker to use a new cache version and improved cache management during activation.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spendlog-v2';
+const CACHE = 'spendlog-v3';
 const SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
@@ -7,7 +7,13 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
